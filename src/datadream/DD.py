949b003c3cbc9ad3f6cbf850fa03c29401c3abb1
gael <gael.bernard@epfl.ch>
2023-12-07 17:30:43 +0100

    Changed path to umap

--- src/datadream/DD.py
+++ src/datadream/DD.py
@@ -255,7 +255,7 @@ class DataPrep:
         if not os.path.exists(self.path):
             os.mkdir(self.path)
 
-    def run(self, embeddings=None, map=None, translate=False, umap_cloud_path='umap_n12_openalex_V2.pkl', delete=False, device=None):
+    def run(self, embeddings=None, map=None, translate=False, umap_cloud_path='umap_n20.pkl', delete=False, device=None):
         '''
         Run the data preparation
         :param translate:u if True, we _translate the text to english.
